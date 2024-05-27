291046c0dc3a957b3aeb577872be80ab6809cfaa
Henrique Galinha <hgalinha1999@hotmail.com>
2024-05-27 14:18:43 +0100

    aula2 e ex1

---
+++ aulas/aula02/index.js
@@ -0,0 +1,3 @@
+console.log('Hello, World!');
+console.log('Hello, "Henrique"!');
+console.log(11, 14, 5, 18.43, 19.27, 'Henrique Galinha'); 
---
+++ exercicios/ex001.js
@@ -0,0 +1,1 @@
+console.log('Olá o meu nome é "Henrique". E estou a aprender JavaScript às', 14.13, 'da tarde.');
