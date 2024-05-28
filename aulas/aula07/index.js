2c5776afc70328a88fa9aaee71a8b2c26db052da
Henrique Galinha <hgalinha1999@hotmail.com>
2024-05-28 08:57:01 +0100

    Create index.js

---
+++ aulas/aula07/index.js
@@ -0,0 +1,7 @@
+// Operadores aritméticos
+// +, -, *, /, 
+const primeiroNumero = 17.21;
+const segundoNumero = 22.29;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
+console.log(typeof primeiroNumero);
