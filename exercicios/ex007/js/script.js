5a8d258d1de856f77c8b17ff2ed8a339de9b8b47
Henrique Galinha <hgalinha1999@hotmail.com>
2024-06-03 19:51:58 +0100

    Update script.js

--- exercicios/ex007/js/script.js
+++ exercicios/ex007/js/script.js
@@ -2,6 +2,8 @@ function meuEscopo() {
     const formulario = document.querySelector('.form');
     const escrever = document.querySelector('.resultado');
 
+    const pessoas = [];
+
     function receberClasses (evento) {
         evento.preventDefault();
 
@@ -10,9 +12,19 @@ function meuEscopo() {
         const peso = formulario.querySelector('.peso');
         const altura = formulario.querySelector('.altura');
 
-        const pessoas = [nome.value, sobreNome.value, peso.value, altura.value];
+        pessoas.push({
+            nome: nome.value, 
+            sobreNome: sobreNome.value, 
+            peso: peso.value, 
+            altura: altura.value
+        });
 
-        escrever.innerHTML += `${pessoas} <br />`;
+        console.log(pessoas);
+        
+        escrever.innerHTML += `Nome: ${nome.value} <br />`;
+        escrever.innerHTML += `Sobre Nome: ${sobreNome.value} <br />`;
+        escrever.innerHTML += `Peso: ${peso.value} <br />`;
+        escrever.innerHTML += `Altura: ${altura.value} <br />`;
     }
 
     formulario.addEventListener('submit', receberClasses);
