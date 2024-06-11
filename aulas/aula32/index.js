f2e0a3ba90aefc47e26ce654f438f775b7d61d63
Henrique Galinha <hgalinha1999@hotmail.com>
2024-06-11 13:11:16 +0100

    aulas 32 e 33

---
+++ aulas/aula32/index.js
@@ -0,0 +1,6 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const [a, b, c, ...resto] = numeros
+
+console.log(a, b, c)
+console.log(resto)
---
+++ aulas/aula33/index.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Henrique',
+    sobrenome: 'Galinha',
+    endereco: {
+        rua: 'Rua Principal n29 - Cabecinha',
+        codPostal: '2475-014'
+    }
+}
+
+console.log(pessoa.nome, pessoa.sobrenome, pessoa.endereco)
