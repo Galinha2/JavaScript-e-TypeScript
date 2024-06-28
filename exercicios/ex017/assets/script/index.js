160b1eee218b543bee36d8e52bb8c38002ab7710
Henrique Galinha <hgalinha1999@hotmail.com>
2024-06-28 13:57:52 +0100

    update historico

--- exercicios/ex017/assets/script/index.js
+++ exercicios/ex017/assets/script/index.js
@@ -1,7 +1,9 @@
-function calcula () {    
+function calcula() {
+    const body = document.querySelector('body');
     const documento = document.querySelector('main');
     const botoes = documento.querySelectorAll('.btn');
-    const display = documento.querySelector('.display')
+    const display = documento.querySelector('.display');
+
     return {
 
         documento,
@@ -13,38 +15,60 @@ function calcula () {
         },
 
         escreve(texto) {
-            console.log(this);
             this.display.value += texto;
         },
 
-        fazconta() {
-            let conta = this.display.value;
+        fazConta(value) {
+            let conta = value;
 
             try {
                 conta = eval(conta);
 
                 if (!conta) {
-                    return this.display.textContent('ERRO')
+                    return this.display.value = ('ERRO');
                 }
 
                 return this.display.value = conta;
             } catch (error) {
-                return this.display.value = 'ERROR';
+                return this.display.value = 'ERRO';
             }
+        },
+
+        criaHistorico() {
+            const barra = document.createElement('section');
+            const criaH1 = document.createElement('h1');
+            const close = document.createElement('button');
+
+            barra.classList.add('barra');
+            body.appendChild(barra);
+            void barra.offsetWidth;
+            barra.classList.add('show');
+
+            criaH1.textContent = 'HISTÓRICO';
+            barra.appendChild(criaH1);
+        
+            close.classList.add('close');
+            close.textContent = 'X';   
+            barra.appendChild(close);
+
+            return barra, criaH1, close;
         },
 
         pegaKey() {
 
             document.addEventListener('click', (event) => {
-            let el = event.target
+                let el = event.target;
 
-            if (el.classList.contains('btn-num')) {
-                return this.escreve(el.textContent);
-            } else if (el.classList.contains('btn-clear')) {
-                return this.display.value = '';
-            } else if (el.classList.contains('btn-igual')) {
-                return this.fazConta();
-            }
+                if (el.classList.contains('btn-num')) {
+                    return this.escreve(el.textContent);
+                } else if (el.classList.contains('btn-clear')) {
+                    return this.display.value = '';
+                } else if (el.classList.contains('btn-igual')) {
+                    let values = this.display.value;
+                    return this.fazConta(values);
+                } else if (el.classList.contains('historico')) {
+                    return this.criaHistorico();
+                }
             });
         }
     };
