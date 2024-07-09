e352d02ac991200ff764d5898f32ab989be55fad
Henrique Galinha <hgalinha1999@hotmail.com>
2024-07-09 17:02:55 +0100

    aula77

--- aulas/aula77/index.js
+++ aulas/aula77/index.js
@@ -1,38 +1,46 @@
-function generateCPF() {
-    let arr = Array.from({ length: 9 }, (x) => Math.floor(Math.random() * 9));
-    console.log(formatCPF(validateCPF(arr).join("")));
-  }
-   
-  function validateCPF(arr, rec) {
-    n =
-      11 -
-      (arr
-        .map((e, i, a) => {
-          return e * (a.length - --i);
-        })
-        .reduce((acm, e) => acm + e) %
-        11);
-    if (n > 9) {
-      arr.push(0);
-    } else {
-      arr.push(n);
-    }
-    if (rec !== true) {
-      validateCPF(arr, true);
-    }
-    return arr;
-  }
-   
-  function formatCPF(s) {
-    return (
-      s.substring(0, 3) +
-      "." +
-      s.substring(3, 6) +
-      "." +
-      s.substring(6, 9) +
-      "-" +
-      s.substring(9, 11)
-    );
-  }
-   
-  generateCPF();
+const verificaNumCpf = [];
+
+function verificaCPF(cpf, contador) {
+  const soma = [];
+  const cpfNum = cpf;
+  const cpfFormatado = cpfNum.replace(/[.-]/g, '');
+  for (let valor of cpfFormatado) {
+    if (verificaNumCpf.length < 9) {
+      verificaNumCpf.push(valor);
+    };
+  };
+  for (let valor of verificaNumCpf) {
+    let valores = valor * contador;
+    soma.push(valores);
+    contador--;
+  };
+  const somaTudo = soma.reduce((acumulador, valor) => {
+    acumulador += valor;
+    return acumulador;
+  });
+  const conta = 11 - (somaTudo % 11);
+  const calculoFinal = conta <= 9 ? conta : 0;
+  verificaNumCpf.splice(verificaNumCpf.length, 0, calculoFinal);
+};
+
+function compara(cpf) {
+  if (verificaNumCpf.join('') === cpf) {
+    console.log('CPF CORRÉTO!');
+  } else {
+    console.log('CPF ERRADO!')
+  };
+};
+
+function mostraFuncToda(cpf) {
+  verificaCPF(cpf, 10);
+  verificaCPF(cpf, 11);
+  verificaNumCpf.splice(3, 0, '.');
+  verificaNumCpf.splice(7, 0, '.');
+  verificaNumCpf.splice(11, 0, '-');
+  const cpfValidado = verificaNumCpf.join('');
+  console.log(`CPF recebido: ${cpf}`);
+  console.log(`CPF após validado: ${cpfValidado}`);
+  compara(cpf);
+};
+
+mostraFuncToda('810.063.269-36');
