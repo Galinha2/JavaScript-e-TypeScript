e8e33944592db14e4d3cc7fd1299b8fea1fc967e
Henrique Galinha <hgalinha1999@hotmail.com>
2024-07-09 16:18:38 +0100

    ex018 completo

--- exercicios/ex018/index.js
+++ exercicios/ex018/index.js
@@ -1,4 +1,5 @@
 const cpf = [];
+const verificaNumCpf = [];
 
 function mostraCpf() {
     criaCPF();
@@ -8,25 +9,21 @@ function mostraCpf() {
     cpf.splice(7, 0, '.');
     cpf.splice(11, 0, '-');
     const cpfInteiro = cpf.join('');
-    console.log(cpfInteiro);
-    const cpfNumber = cpfInteiro.replace(/[.-]/g, '');
-    const cpfToString = parseInt(cpfNumber, 10);
-    console.log(cpfToString)
-    console.log(typeof(cpfToString));
+    cpf.splice(0, cpf.length, cpfInteiro);
+    console.log(`CPF recebido: ${cpfInteiro}`);
 };
 
-function criaCPF () {
+function criaCPF() {
     while (cpf.length < 9) {
         let numero = Math.round(Math.random() * 9);
         cpf.push(numero);
     };
 };
 
-function validaCpf (contador) {
-    this.contador = contador;
+function validaCpf(contador) {
     let soma = [];
     for (let valor of cpf) {
-        const calculo = valor*contador;
+        const calculo = valor * contador;
         contador--;
         soma.push(calculo);
     };
@@ -35,8 +32,51 @@ function validaCpf (contador) {
         return acumulador;
     });
     const calculoTotal = 11 - (somaTotal % 11);
-    const calculoFinal = calculoTotal <= 9 ? calculoTotal : 0; 
+    const calculoFinal = calculoTotal <= 9 ? calculoTotal : 0;
     cpf.splice(cpf.length, 0, calculoFinal);
 };
 
-mostraCpf();
+function verificaCPF(contador) {
+    const soma = [];
+    const cpfNum = cpf[0];
+    const cpfFormatado = cpfNum.replace(/[.-]/g, '');
+    for (let valor of cpfFormatado) {
+        if (verificaNumCpf.length < 9) {
+            verificaNumCpf.push(valor);
+        };
+    };
+    for (let valor of verificaNumCpf) {
+        let valores = valor * contador;
+        soma.push(valores);
+        contador--;
+    };
+    const somaTudo = soma.reduce((acumulador, valor) => {
+        acumulador += valor;
+        return acumulador;
+    });
+    const conta = 11 - (somaTudo % 11);
+    const calculoFinal = conta <= 9 ? conta : 0;
+    verificaNumCpf.splice(verificaNumCpf.length, 0, calculoFinal);
+};
+
+function compara() {
+    if (verificaNumCpf.join('') === cpf[0]) {
+        console.log('CPF CORRÉTO!');
+    } else {
+        console.log('CPF ERRADO!')
+    };
+};
+
+function mostraFuncToda() {
+    mostraCpf();
+    verificaCPF(10);
+    verificaCPF(11);
+    verificaNumCpf.splice(3, 0, '.');
+    verificaNumCpf.splice(7, 0, '.');
+    verificaNumCpf.splice(11, 0, '-');
+    const cpfValidado = verificaNumCpf.join('');
+    console.log(`CPF após validado: ${cpfValidado}`);
+    compara();
+};
+
+mostraFuncToda();
