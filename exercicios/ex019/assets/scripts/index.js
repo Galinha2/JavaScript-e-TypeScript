265e485719875971919c3114289a51a7e86edf1f
Henrique Galinha <hgalinha1999@hotmail.com>
2024-07-16 17:02:32 +0100

    ex19 almost

--- exercicios/ex019/assets/scripts/index.js
+++ exercicios/ex019/assets/scripts/index.js
@@ -2,35 +2,58 @@ class Form {
     constructor() {
         this.main = document.querySelector('main')
     };
-    
+
     pegaClique() {
         document.addEventListener('click', (event) => {
             let clic = event.target;
-
-            if(clic.classList.contains('send')) {
-                event.preventDefault();
-                const inputs = this.main.querySelectorAll('input');
-               inputs.forEach(element => {
-                    console.log(element.value);
             
-                const nome = this.main.querySelector('.nome');
-                const contenudoNome = nome.value;
-           
-                if (contenudoNome.length > 5) {
-                    const nomes = document.createElement('p');
-                    nomes.classList.add('teste');
-                    nomes.textContent = 'oiiiiiiii';
-                    this.main.innerHTML += 'muito grande!';
+            if (clic.classList.contains('send')) {
+                event.preventDefault();
+                let nome = this.escolheClasse('.nome');
+                let apelido = this.escolheClasse('.apelido');
+                let cpf = this.escolheClasse('.cpf');
+                let user = this.escolheClasse('.usuario');
+                let senha = this.escolheClasse('.senha');
+                let repetir = this.escolheClasse('.repetirSenha');
+
+                if (nome.length === 0 ) {
+                    this.escreveErro('.nomeErrado', 'Precisa de introduzir um nome!');
                 };
-               });
+                if (apelido.length === 0) {
+                    this.escreveErro('.apelidoErrado', 'Precisa de introduzir um apelido!')
+                };
+                if (user.length < 3 || user.length > 12) {
+                    this.escreveErro('.userErrado', 'Usuário tem de ter entre 3 a 12 letras!')
+                    console.log(senha)
+                };
+                if (senha !== repetir) {
+                    this.escreveErro('.repetirErrado', 'As senhas não coincidem!')
+                };
+
+                if (senha < 6 || senha > 12) {
+                    this.escreveErro('.senhaErrado', 'Senha tem de ter entre 3 a 12 letras!')
+                };
             };
         });
+    };
+
+    escolheClasse(classe) {
+        const nome = this.main.querySelector(classe);
+        return nome.value;
+    };
+
+    escreveErro(classe, erro) {
+        const nome = document.querySelector(classe);
+        const p = document.createElement('p');
+        p.classList.add('erro');
+        p.textContent = erro;
+        nome.append(p);
     };
 
     nome(texto) {
         console.log(texto);
     };
-    
+
 };
 
 const form = new Form();
